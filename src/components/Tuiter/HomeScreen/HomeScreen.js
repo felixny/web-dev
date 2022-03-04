f1ba126433d7d88c889ff5188d2aa34c2533f220
Felix <nampanya.f@northeastern.edu>
2022-03-04 15:13:24 -0500

    edited homescreen

--- src/components/Tuiter/HomeScreen/HomeScreen.js
+++ src/components/Tuiter/HomeScreen/HomeScreen.js
@@ -9,22 +9,22 @@ import "./home.css"
 const HomeScreen = () => {
     return (
         <>
-            <div class="row mt-2">
+            <div className="row mt-2">
 
-                <div class="col-2 col-md-2 col-lg-1 col-xl-2">
+                <div className="col-2 col-md-2 col-lg-1 col-xl-2">
                     <NavigationSidebar active="home" />
                 </div>
 
-                <div class="col-10 col-md-10 col-lg-7 col-xl-6">
+                <div className="col-10 col-md-10 col-lg-7 col-xl-6">
                     <HomeComponent />
                 </div>
 
-                <div class="d-sm-none d-md-none d-lg-block col-lg-4 col-xl-4 wd-ps-col3">
+                <div className="d-sm-none d-md-none d-lg-block col-lg-4 col-xl-4 wd-ps-col3">
                     <PostSummaryList />
 
                 </div>
 
-                <div class="mt-4"></div>
+                <div className="mt-4"></div>
 
             </div>
         </>
